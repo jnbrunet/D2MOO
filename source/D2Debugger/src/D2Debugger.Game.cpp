ba227993026a2c5a49bfa7a7199180595f9f5990
Jean-Nicolas Brunet <jnbrunet2000@gmail.com>
2026-04-13 21:58:49 -0400

    Item dropper

--- source/D2Debugger/src/D2Debugger.Game.cpp
+++ source/D2Debugger/src/D2Debugger.Game.cpp
@@ -7,6 +7,7 @@
 #include <D2Dll.h>
 #include <D2DataTbls.h>
 #include <D2StatList.h>
+#include <D2Items.h>
 
 #include "IconsFontAwesome6.h"
 
@@ -23,6 +24,8 @@ HMODULE delayedD2GameDllBaseGet()
 static const int D2GameImageBase = 0x6FC30000;
 D2FUNC(D2Game, SpawnSuperUnique_6FC6F690, D2UnitStrc*, __fastcall, (D2GameStrc* pGame, D2ActiveRoomStrc* pRoom, int32_t nX, int32_t nY, int32_t nSuperUnique), 0x6FC6F690 - D2GameImageBase);
 D2FUNC(D2Game, SpawnMonster_6FC69F10, D2UnitStrc*, __fastcall, (D2GameStrc* pGame, D2ActiveRoomStrc* pRoom, int32_t nX, int32_t nY, int32_t nMonsterId, int32_t nAnimMode, int32_t a7, int16_t nFlags), 0x6FC69F10 - D2GameImageBase);
+D2FUNC(D2Game, CreateItemUnit_6FC501A0, D2UnitStrc*, __fastcall, (D2UnitStrc* pPlayer, int32_t nItemId, D2GameStrc* pGame, int32_t nSpawnTarget, int32_t nQuality, int32_t bNoSockets, int32_t bNoEthereal, int32_t nItemLevel, int32_t bUseSeed, int32_t dwSeed, int32_t dwItemSeed), 0x6FC501A0 - D2GameImageBase);
+D2FUNC(D2Game, DropItem_6FC52260, void, __fastcall, (D2GameStrc* pGame, D2UnitStrc* pUnit, D2UnitStrc* pItem, D2ActiveRoomStrc* pRoom, int32_t nX, int32_t nY), 0x6FC52260 - D2GameImageBase);
 #elif defined(D2_VERSION_114D)
 #else
 #pragma message("Warning: Unsupported D2 version for D2Debugger")
@@ -369,6 +372,260 @@ void D2DebugUnitSpawner(D2GameStrc* pGame)
 }
 
 static bool bFreezeGame = false;
+static bool ContainsInsensitive(const char* pStr, const char* pSearch)
+{
+    if (!pSearch || pSearch[0] == '\0') return true;
+    if (!pStr) return false;
+    const size_t nSearchLen = strlen(pSearch);
+    for (; *pStr; ++pStr)
+    {
+        if (_strnicmp(pStr, pSearch, nSearchLen) == 0)
+            return true;
+    }
+    return false;
+}
+
+void D2DebugItemDropper(D2GameStrc* pGame)
+{
+#ifdef HAS_SPAWN_FUNCTIONS
+    if (!ImGui::CollapsingHeader("Item Dropper"))
+        return;
+
+    D2UnitStrc* pPlayer = GetFirstPlayerInList(pGame);
+    if (!pPlayer)
+    {
+        ImGui::TextDisabled("No player in game");
+        return;
+    }
+
+    D2CoordStrc tCoords{};
+    UNITS_GetCoords(pPlayer, &tCoords);
+    ImGui::Text("Drop location (player): (%d, %d)", tCoords.nX, tCoords.nY);
+
+    // --- Item type filter ---
+    struct ItemTypeEntry { const char* szName; int nTypeId; };
+    static const ItemTypeEntry gItemTypeFilters[] = {
+        { "All",              -1                     },
+        { "Any Armor",        ITEMTYPE_ANY_ARMOR      },
+        { "Body Armor",       ITEMTYPE_ARMOR          },
+        { "Helm",             ITEMTYPE_HELM           },
+        { "Boots",            ITEMTYPE_BOOTS          },
+        { "Gloves",           ITEMTYPE_GLOVES         },
+        { "Belt",             ITEMTYPE_BELT           },
+        { "Any Shield",       ITEMTYPE_ANY_SHIELD     },
+        { "Shield",           ITEMTYPE_SHIELD         },
+        { "Auric Shield",     ITEMTYPE_AURIC_SHIELDS  },
+        { "Any Weapon",       ITEMTYPE_WEAPON         },
+        { "Melee Weapon",     ITEMTYPE_MELEE_WEAPON   },
+        { "Axe",              ITEMTYPE_AXE            },
+        { "Sword",            ITEMTYPE_SWORD          },
+        { "Club",             ITEMTYPE_CLUB           },
+        { "Hammer",           ITEMTYPE_HAMMER         },
+        { "Mace",             ITEMTYPE_MACE           },
+        { "Knife",            ITEMTYPE_KNIFE          },
+        { "Spear",            ITEMTYPE_SPEAR          },
+        { "Polearm",          ITEMTYPE_POLEARM        },
+        { "Scepter",          ITEMTYPE_SCEPTER        },
+        { "Wand",             ITEMTYPE_WAND           },
+        { "Staff",            ITEMTYPE_STAFF          },
+        { "Orb",              ITEMTYPE_ORB            },
+        { "Voodoo Head",      ITEMTYPE_VOODOO_HEADS   },
+        { "Primal Helm",      ITEMTYPE_PRIMAL_HELM    },
+        { "Pelt",             ITEMTYPE_PELT           },
+        { "Cloak",            ITEMTYPE_CLOAK          },
+        { "Circlet",          ITEMTYPE_CIRCLET        },
+        { "Missile Weapon",   ITEMTYPE_MISSILE_WEAPON },
+        { "Bow",              ITEMTYPE_BOW            },
+        { "Crossbow",         ITEMTYPE_CROSSBOW       },
+        { "Ring",             ITEMTYPE_RING           },
+        { "Amulet",           ITEMTYPE_AMULET         },
+        { "Charm",            ITEMTYPE_CHARM          },
+        { "Rune",             ITEMTYPE_RUNE           },
+        { "Gem",              ITEMTYPE_GEM            },
+        { "Jewel",            ITEMTYPE_JEWEL          },
+        { "Potion",           ITEMTYPE_POTION         },
+        { "Miscellaneous",    ITEMTYPE_MISCELLANEOUS  },
+    };
+
+    // --- Quality ---
+    struct QualityEntry { const char* szName; D2C_ItemQualities eQuality; };
+    static const QualityEntry gQualities[] = {
+        { "Normal",   ITEMQUAL_NORMAL   },
+        { "Superior", ITEMQUAL_SUPERIOR },
+        { "Magic",    ITEMQUAL_MAGIC    },
+        { "Rare",     ITEMQUAL_RARE     },
+        { "Unique",   ITEMQUAL_UNIQUE   },
+        { "Set",      ITEMQUAL_SET      },
+    };
+
+    static int nTypeFilterIdx   = 0;
+    static int nQualityIdx      = 0;
+    static int nItemLevel       = 99;
+    static int nSelectedItemId  = 0;
+    static char szSearch[64]    = {};
+
+    // Row 1: type, quality, item level
+    ImGui::SetNextItemWidth(150.f);
+    if (ImGui::BeginCombo("Type##ItemDrop", gItemTypeFilters[nTypeFilterIdx].szName))
+    {
+        for (int i = 0; i < ARRAY_SIZE(gItemTypeFilters); ++i)
+        {
+            const bool bSel = (nTypeFilterIdx == i);
+            if (ImGui::Selectable(gItemTypeFilters[i].szName, bSel))
+                nTypeFilterIdx = i;
+            if (bSel) ImGui::SetItemDefaultFocus();
+        }
+        ImGui::EndCombo();
+    }
+
+    ImGui::SameLine();
+    ImGui::SetNextItemWidth(100.f);
+    if (ImGui::BeginCombo("Quality##ItemDrop", gQualities[nQualityIdx].szName))
+    {
+        for (int i = 0; i < ARRAY_SIZE(gQualities); ++i)
+        {
+            const bool bSel = (nQualityIdx == i);
+            if (ImGui::Selectable(gQualities[i].szName, bSel))
+                nQualityIdx = i;
+            if (bSel) ImGui::SetItemDefaultFocus();
+        }
+        ImGui::EndCombo();
+    }
+
+    ImGui::SameLine();
+    ImGui::SetNextItemWidth(60.f);
+    ImGui::InputInt("ilvl##ItemDrop", &nItemLevel, 0, 0);
+    if (nItemLevel < 1)  nItemLevel = 1;
+    if (nItemLevel > 99) nItemLevel = 99;
+
+    // Row 2: text search
+    ImGui::SetNextItemWidth(300.f);
+    ImGui::InputText("Search##ItemDrop", szSearch, sizeof(szSearch));
+
+    // Build filtered item list every frame (debug tool, perf not critical)
+    static std::vector<int> s_FilteredItems;
+    s_FilteredItems.clear();
+
+    const D2ItemDataTbl* pItemTbl = DATATBLS_GetItemDataTables();
+    const int nTypeId = gItemTypeFilters[nTypeFilterIdx].nTypeId;
+
+    for (int i = 0; i < pItemTbl->nItemsTxtRecordCount; ++i)
+    {
+        D2ItemsTxt* pRec = DATATBLS_GetItemsTxtRecord(i);
+        if (!pRec)
+            continue;
+
+        // Type filter
+        if (nTypeId >= 0 && !ITEMS_CheckItemTypeIdByItemId(i, nTypeId))
+            continue;
+
+        // Name search filter
+        if (szSearch[0] != '\0')
+        {
+            auto nameBuf = GetUTF8CharBufferFromStringIndex(pRec->wNameStr);
+            if (!ContainsInsensitive(nameBuf.data(), szSearch))
+                continue;
+        }
+
+        s_FilteredItems.push_back(i);
+    }
+
+    // Row 3: item combo + drop button
+    // Build preview string for currently selected item
+    static std::vector<char> s_PreviewBuf;
+    {
+        D2ItemsTxt* pRec = DATATBLS_GetItemsTxtRecord(nSelectedItemId);
+        if (pRec)
+        {
+            auto tmp = GetUTF8CharBufferFromStringIndex(pRec->wNameStr);
+            // Format as "Name (CODE)"
+            char szCode[5] = { pRec->szCode[0], pRec->szCode[1], pRec->szCode[2], pRec->szCode[3], '\0' };
+            // Trim trailing spaces from code
+            for (int k = 3; k >= 0 && szCode[k] == ' '; --k) szCode[k] = '\0';
+            const int nNeeded = (int)(tmp.size() + 8);
+            s_PreviewBuf.resize(nNeeded);
+            _snprintf_s(s_PreviewBuf.data(), nNeeded, _TRUNCATE, "%s (%s)", tmp.data(), szCode);
+        }
+        else
+        {
+            static const char szNone[] = "(select an item)";
+            s_PreviewBuf.assign(szNone, szNone + sizeof(szNone));
+        }
+    }
+
+    ImGui::Text("Matches: %d", (int)s_FilteredItems.size());
+    ImGui::SetNextItemWidth(350.f);
+    if (ImGui::BeginCombo("Base Item##ItemDrop", s_PreviewBuf.data()))
+    {
+        ImGuiListClipper clipper;
+        clipper.Begin((int)s_FilteredItems.size());
+        while (clipper.Step())
+        {
+            for (int i = clipper.DisplayStart; i < clipper.DisplayEnd; ++i)
+            {
+                const int nItemId = s_FilteredItems[i];
+                D2ItemsTxt* pRec = DATATBLS_GetItemsTxtRecord(nItemId);
+                if (!pRec) continue;
+
+                auto nameBuf = GetUTF8CharBufferFromStringIndex(pRec->wNameStr);
+                char szCode[5] = { pRec->szCode[0], pRec->szCode[1], pRec->szCode[2], pRec->szCode[3], '\0' };
+                for (int k = 3; k >= 0 && szCode[k] == ' '; --k) szCode[k] = '\0';
+
+                char szLabel[256];
+                _snprintf_s(szLabel, sizeof(szLabel), _TRUNCATE, "%s (%s)##%d", nameBuf.data(), szCode, nItemId);
+
+                const bool bSel = (nSelectedItemId == nItemId);
+                if (ImGui::Selectable(szLabel, bSel))
+                    nSelectedItemId = nItemId;
+                if (bSel)
+                    ImGui::SetItemDefaultFocus();
+            }
+        }
+        clipper.End();
+        ImGui::EndCombo();
+    }
+
+    ImGui::SameLine();
+    if (ImGui::Button("Drop##ItemDrop"))
+    {
+        if (!pPlayer->pDynamicPath)
+        {
+            ImGui::OpenPopup("ItemDropFailed##ItemDrop");
+        }
+        else
+        {
+            D2UnitStrc* pItem = D2Game_CreateItemUnit_6FC501A0(
+                pPlayer, nSelectedItemId, pGame,
+                /*nSpawnTarget=*/3,
+                (int32_t)gQualities[nQualityIdx].eQuality,
+                /*bNoSockets=*/0, /*bNoEthereal=*/0,
+                nItemLevel,
+                /*bUseSeed=*/0, /*dwSeed=*/0, /*dwItemSeed=*/0);
+            if (pItem)
+            {
+                D2Game_DropItem_6FC52260(pGame, pPlayer, pItem,
+                    pPlayer->pDynamicPath->pRoom, tCoords.nX, tCoords.nY);
+            }
+            else
+            {
+                ImGui::OpenPopup("ItemDropFailed##ItemDrop");
+            }
+        }
+    }
+
+    bool bPopupOpen = true;
+    if (ImGui::BeginPopupModal("ItemDropFailed##ItemDrop", &bPopupOpen))
+    {
+        ImGui::Text("Item creation failed.");
+        ImGui::BulletText("The item/quality combination may be invalid.");
+        ImGui::BulletText("The item may not support the selected quality.");
+        if (ImGui::Button("Close"))
+            ImGui::CloseCurrentPopup();
+        ImGui::EndPopup();
+    }
+#endif
+}
+
 bool D2DebugGame(D2GameStrc* pGame)
 {
     if(ImGui::Begin("Game"))
@@ -408,6 +665,7 @@ bool D2DebugGame(D2GameStrc* pGame)
         ImGui::BulletText("Tiles: %d", pGame->dwLastUsedUnitGUID[UNIT_TILE]);
 
         D2DebugUnitSpawner(pGame);
+        D2DebugItemDropper(pGame);
 
         if (ImGui::CollapsingHeader("Players units"))
         {
